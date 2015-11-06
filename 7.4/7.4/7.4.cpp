4037aaac4f6321a298551908255960b50addbbbf
leshadeking <mizantropdeking@gmail.com>
2015-11-06 21:05:07 +0300

    ignore+modifed

--- 7.4/7.4/7.4.cpp
+++ 7.4/7.4/7.4.cpp
@@ -2,19 +2,19 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int n,i,k;
-	cout << "Zadaite razmer massiva";
+	cout << "Zadaite razmer massiva" << endl;
 	cin >> n;
 	int *a =new int[n];
 	for (int i = 0; i < n; i++)
 	a[i]=rand()%100;
 	int max = a[0];
-	cout << "Push 'k'";
+	cout << "Push 'k'" << endl;
 	cin >> k;
 	for (i=1; i<n; i++)
 	{
 		if (a[i]%k==0)
 		{
-			cout << i;
+			cout << i << endl;
 			if (a[i]>max) 
 			{
 				max=a[i];
@@ -22,7 +22,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 	cout << max;
-	getchar();
+	getch();
 	return 0;
 }
 
